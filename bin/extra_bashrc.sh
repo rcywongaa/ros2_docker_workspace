fc9a1b6ddcec0c227f5337bca19cb554d4c74d02
Rufus Wong <rcywongaa@gmail.com>
2022-06-06 00:55:50 +0200

    Source workspace setup.bash by default

--- bin/extra_bashrc.sh
+++ bin/extra_bashrc.sh
@@ -2,4 +2,5 @@ DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> 
 
 source "${DIR}"/config.sh
 source /opt/ros/"${ROS_DISTRO}"/setup.bash
+source /${WORKSPACE_NAME}/install/setup.bash
 alias run="source /${WORKSPACE_NAME}/install/setup.bash && ros2 launch dual_arm_husky_description main_launch.py"

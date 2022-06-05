4df4e3d24abd48d98bf2eba95c9f25247f996576
Rufus Wong <rcywongaa@gmail.com>
2022-06-05 19:20:45 +0200

    Fix command to get script directory

--- bin/enter_container.sh
+++ bin/enter_container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DIR=$(dirname "$(readlink -f "$0")")
+DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 source "${DIR}"/config.sh
 

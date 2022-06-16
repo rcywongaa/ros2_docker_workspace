b262de0396e45296b4846de3011bfc4e277ab0eb
Rufus Wong <rcywongaa@gmail.com>
2022-06-16 11:29:44 +0200

    Fix start_container.sh

--- bin/start_container.sh
+++ bin/start_container.sh
@@ -3,13 +3,15 @@ DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> 
 
 source "${DIR}"/config.sh
 
+# privileged is required for gdb
+
 rocker \
     --nvidia \
     --x11 \
     --volume="${DIR}/..":"/${WORKSPACE_NAME}":rw \
     --user \
     --name "${CONTAINER_NAME}" \
-    --privileged \ # required for gdb
+    --privileged \
     -- \
     "${IMAGE_NAME}" \
     bash
@@ -20,7 +22,7 @@ rocker \
      #--env="QT_X11_NO_MITSHM=1" \
      #--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
      #--volume="${DIR}/..":"/${WORKSPACE_NAME}":rw \
-     #--privileged \ # for gdb
-     #-u $(id -u ${USER}):$(id -g ${USER})
+     #--privileged \
+     #-u $(id -u ${USER}):$(id -g ${USER}) \
      #"${IMAGE_NAME}" \
      #bash

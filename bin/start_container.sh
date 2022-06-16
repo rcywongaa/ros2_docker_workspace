319539e2d248317831df6c49e66c1c8910ea4432
Rufus Wong <rcywongaa@gmail.com>
2022-06-16 10:44:33 +0200

    Add equivalent docker command of rocker

--- bin/start_container.sh
+++ bin/start_container.sh
@@ -14,11 +14,13 @@ rocker \
     "${IMAGE_NAME}" \
     bash
 
-# docker run -it --net=host --gpus all \
-#     --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-#     --env="DISPLAY" \
-#     --env="QT_X11_NO_MITSHM=1" \
-#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#     --volume="${DIR}/..":"/${WORKSPACE_NAME}":rw \
-#     "${IMAGE_NAME}" \
-#     bash
+ #docker run -it --net=host --gpus all \
+     #--env="NVIDIA_DRIVER_CAPABILITIES=all" \
+     #--env="DISPLAY" \
+     #--env="QT_X11_NO_MITSHM=1" \
+     #--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+     #--volume="${DIR}/..":"/${WORKSPACE_NAME}":rw \
+     #--privileged \ # for gdb
+     #-u $(id -u ${USER}):$(id -g ${USER})
+     #"${IMAGE_NAME}" \
+     #bash
